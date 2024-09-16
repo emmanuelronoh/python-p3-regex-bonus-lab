728929f231555fefe3ece5b5da6fbacbf6641984
emmanuel ronoh <eronoh036@gmail.com>
2024-09-16 17:08:08 +0300

    first commit

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,11 @@
 import re
 
-my_pattern = r""
-my_regex = re.compile(my_pattern)
+# Define the regular expression pattern
+pattern = re.compile(
+    r"It's such a lovely day today\."  # Match "It's such a lovely day today."
+    r"|Some weather we're having today, huh\?"  # Match "Some weather we're having today, huh?"
+    r"|Maybe today's just not my day\."  # Match "Maybe today's just not my day."
+)
 
+# Create a regex object with the pattern
+my_regex = pattern
